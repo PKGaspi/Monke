ae9fef1c4cc9d96c6e96f96455827f04880e6c15
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2022-01-12 21:09:33 +0100

    Added controls for gadets

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -2,34 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
-public class ThirdPersonMovement : MonoBehaviour
-{
+public class ThirdPersonMovement : MonoBehaviour {
     public const int AERIAL_JUMPS = 1;
 
     public CharacterController controller;
     public Transform cam;
+    public CinemachineInputProvider camInput;
+    public PlayerInput playerInput;
 
     public Vector3 velocity;
     private Vector3 movementDir = Vector3.zero;
+    private Vector3 gadgetDir = Vector3.zero;
     private float gravity = -15.0f;
     private float walkSpeed = 3.0f;
     private float jumpSpeed = 6.0f;
     private float aerialJumpSpeed = 8.0f;
+    private bool canUseGadget = false;
+    private InputActionReference defaultXYAxis;
     public int aerialJumpsLeft = AERIAL_JUMPS;
 
 
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        //controller = gameObject.GetComponent(typeof(CharacterController)) as CharacterController;
+    void Start() {
+        defaultXYAxis = camInput.XYAxis;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
+
         if (controller.isGrounded) {
             aerialJumpsLeft = AERIAL_JUMPS;
         }
@@ -47,6 +51,11 @@ public class ThirdPersonMovement : MonoBehaviour
     }
 
     public void OnMovement(InputAction.CallbackContext value) {
+        if (gadgetDir != Vector3.zero) {
+            // Use gadget and not move.
+            return;
+        }
+
         Vector2 inputMovementDir = value.ReadValue<Vector2>();
         movementDir = Quaternion.Euler(0f, cam.eulerAngles.y, 0f) * new Vector3(inputMovementDir.x, 0f, inputMovementDir.y);
 
@@ -57,7 +66,7 @@ public class ThirdPersonMovement : MonoBehaviour
     }
 
     public void OnJump(InputAction.CallbackContext value) {
-        if (value.started && aerialJumpsLeft > 0) {
+        if (value.started && aerialJumpsLeft > 0 && gadgetDir == Vector3.zero) {
             if (!controller.isGrounded) {
                 // Remove aerial jump.
                 aerialJumpsLeft--;
@@ -66,6 +75,47 @@ public class ThirdPersonMovement : MonoBehaviour
             else {
                 velocity.y = jumpSpeed;
             }
+        }
+    }
+
+    public void OnGadget(InputAction.CallbackContext value) {
+        if (!canUseGadget) {
+            return;
+        }
+
+        // Gadget usage is canceled.
+        if (value.canceled) {
+            gadgetDir = Vector3.zero;
+        }
+
+        // Get gadget dir and cancel movement.
+        movementDir = Vector3.zero;
+        Vector2 inputGadgetDir = value.ReadValue<Vector2>();
+        gadgetDir = Quaternion.Euler(0f, cam.eulerAngles.y, 0f) * new Vector3(inputGadgetDir.x, 0f, inputGadgetDir.y);
+
+        // Use gadget
+        if (gadgetDir != Vector3.zero) {
+            transform.forward = gadgetDir;
+        }
+    }
+
+    public void OnGadgetCameraToggle(InputAction.CallbackContext value) {
+        if (value.performed) {
+            return;
+        }
+        // Hold for gadget, release for camera.
+        if (value.started) {
+            // Set Gadget and disable camera.
+            canUseGadget = true;
+            playerInput.actions["Look"].Disable();
+            camInput.XYAxis = null;
+        }
+        else if (value.canceled) {
+            // Set Camera and disable camera.
+            gadgetDir = Vector3.zero;
+            canUseGadget = false;
+            camInput.XYAxis = defaultXYAxis;
+            playerInput.actions["Look"].Enable();
         }
     }
 }
