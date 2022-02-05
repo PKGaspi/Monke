cbc9a0b178866da2c460a553b443296e5ddb4957
Juan Gaspar García <24842781+PKGaspi@users.noreply.github.com>
2022-02-05 01:25:20 +0100

    Added music

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -4,6 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour
 {
+
+    public AudioSource music;
+
+    public void Start() {
+        music.Play();
+    }
     
     public void OnGameStart(InputAction.CallbackContext value) {
         if (value.started) {
